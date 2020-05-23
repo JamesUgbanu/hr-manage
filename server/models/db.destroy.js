40bab43bc44e74942cd78c7bee28f6eee9555b09
babatunde tunde <tunde8983@gmail.com>
2020-05-23 07:21:01 +0100

    feat(Sign up: Admin can signup user and registered user can login [Finishes #170916762])

--- server/models/db.destroy.js
+++
@@ -1,7 +0,0 @@
-const createExt = 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; ';
-const userDestroy = 'DROP TABLE IF EXISTS users CASCADE; ';
-const leaveTable = 'DROP TABLE IF EXISTS leaverequests CASCADE; ';
-
-const destroyQuery = `${userDestroy}${leaveTable}${createExt}`;
-
-export default destroyQuery;
